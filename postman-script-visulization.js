5043b8faeb64f12cdaa19ad3dae9ce90298044a5
Dileep Donepudi <donepudi4u@gmail.com>
2026-04-21 00:35:56 -0400

    Change app selection from link to button
    Refactor app selection to use a button instead of a link.

--- postman-script-visulization.js
+++ postman-script-visulization.js
@@ -4,13 +4,13 @@ let selected = pm.environment.get("selected_app");
 let apps = Object.keys(clients).map(app => {
     return {
         name: app,
-        selected: app === selected,
-        link: `?app=${app}`
+        selected: app === selected
     };
 });
 
 let template = `
 <h2>Developer Apps</h2>
+
 <table border="1" cellpadding="8" cellspacing="0">
     <tr>
         <th>App Name</th>
@@ -22,13 +22,22 @@ let template = `
         <td>{{name}}</td>
         <td>{{#if selected}}✅ Selected{{/if}}</td>
         <td>
-            <a href="{{link}}">Select</a>
+            <button onclick="selectApp('{{name}}')">Select</button>
         </td>
     </tr>
     {{/each}}
 </table>
 
 <p><b>Current Selected App:</b> {{selected}}</p>
+
+<script>
+function selectApp(appName) {
+    pm.environment.set("selected_app", appName);
+    alert("Selected app updated to: " + appName);
+
+    // Optional: auto re-run request (manual refresh needed otherwise)
+}
+</script>
 `;
 
 pm.visualizer.set(template, { apps, selected });
