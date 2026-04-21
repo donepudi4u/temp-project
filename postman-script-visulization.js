6ab4a45208428769a1d58f1f6961fe42cad4f8b3
Dileep Donepudi <donepudi4u@gmail.com>
2026-04-21 00:32:03 -0400

    Enhance visualization template with action links

--- postman-script-visulization.js
+++ postman-script-visulization.js
@@ -4,24 +4,31 @@ let selected = pm.environment.get("selected_app");
 let apps = Object.keys(clients).map(app => {
     return {
         name: app,
-        selected: app === selected ? "✅ Selected" : ""
+        selected: app === selected,
+        link: `?app=${app}`
     };
 });
 
 let template = `
-    <h2>Available Developer Apps</h2>
-    <table border="1" cellpadding="8" cellspacing="0">
-        <tr>
-            <th>App Name</th>
-            <th>Status</th>
-        </tr>
-        {{#each apps}}
-        <tr>
-            <td>{{name}}</td>
-            <td>{{selected}}</td>
-        </tr>
-        {{/each}}
-    </table>
+<h2>Developer Apps</h2>
+<table border="1" cellpadding="8" cellspacing="0">
+    <tr>
+        <th>App Name</th>
+        <th>Status</th>
+        <th>Action</th>
+    </tr>
+    {{#each apps}}
+    <tr>
+        <td>{{name}}</td>
+        <td>{{#if selected}}✅ Selected{{/if}}</td>
+        <td>
+            <a href="{{link}}">Select</a>
+        </td>
+    </tr>
+    {{/each}}
+</table>
+
+<p><b>Current Selected App:</b> {{selected}}</p>
 `;
 
-pm.visualizer.set(template, { apps });
+pm.visualizer.set(template, { apps, selected });
